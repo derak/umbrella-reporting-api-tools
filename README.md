3a802473e5161e81d92cb7113c8e085e4d65a541
Derak Berreyesa <derak@opendns.com>
2020-05-09 19:58:46 -0700

    add ability to search by application name

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ Tools to interact with the Cisco Umbrella Reporting API V2.
 Add your management API key and secret as well as OrgID to the `config` file. These scripts use the Umbrella management API to create a temporary access token that is then used to call the v2 reporting API. You do not need to create a reporting API key and secret.
 
 ## Get a list of identities by category type
-You can change the category type that you want to get identities for by editing the `SearchCatagory` parameter in the `config` file.
+You can change the category type that you want to get identities for by editing the `SearchCategory` parameter in the `config` file.
 
 The example below will searh for all identities with requests to the `File Storage` category:
 ```
 [SearchOptions]
-SearchCatagory = File Storage
+SearchCategory = File Storage
 ```
 
 Run with Python 3 like so:
--- get-identities.py
+++ get-identities.py
@@ -8,7 +8,10 @@ config.read('config')
 org_id = config['Umbrella']['OrgID']
 mgmt_api_key = config['Umbrella']['ManagementAPIKey']
 mgmt_api_secret = config['Umbrella']['ManagementAPISecret']
-category_type = config['SearchOptions']['SearchCatagory']
+
+search_type = config['SearchOptions']['Type']
+category_type = config['SearchOptions']['Category']
+application_name = config['SearchOptions']['Application']
 
 header = {'content-type': 'application/json'}
 
@@ -32,21 +35,40 @@ def get_access_token():
 
 if __name__ == '__main__':
 
+    if search_type.lower() == 'category':
+        search_param = category_type
+        list_endpoint = 'categories'
+        type_param = 'categories'
+        help_text = 'category type'
+    elif search_type.lower() == 'application':
+        search_param = application_name
+        list_endpoint = 'applications'
+        type_param = 'applicationid'
+        help_text = 'application name'
+    else:
+        print('\nError, unknown search type. Use either Category or Application in config file.')
+        exit(1)
+
     # get access token
     access_token = get_access_token()
 
-    # get list of all categories
-    r = get_reporting_request(access_token, '/organizations/{}/categories'.format(org_id))
+    # get list of all categories or applications
+    r = get_reporting_request(access_token, '/organizations/{}/{}'.format(org_id, list_endpoint))
 
-    # loop througth category list and get category id
-    category_id = ''
+    # loop througth categories or applications list and get id
+    search_id = ''
     for i in r['data']:
-        if i['label'] == category_type:
-            category_id = i['id']
-            break
+        if 'label' in i:
+            if i['label'].lower() == search_param.lower():
+                search_id = i['id']
+                break
+
+    if search_id == '':
+        print('\nError, unknown {}, "{}" in config file.'.format(help_text, search_param))
+        exit(1)
 
     # get identities for specific category
-    r = get_reporting_request(access_token, '/organizations/{}/top-identities?from=-30days&to=now&limit=1000&offset=0&categories={}'.format(org_id, category_id))
+    r = get_reporting_request(access_token, '/organizations/{}/top-identities?from=-30days&to=now&limit=1000&offset=0&{}={}'.format(org_id, type_param, search_id))
 
     # format row for printing in columns
     row_format = '{:50} {:20} {}'
